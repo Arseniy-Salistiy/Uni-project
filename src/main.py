42258b3a8c3ffe9e4e64627944923cbd567910e0
Arseniy-Salistiy <salistyjarseniy@gmail.com>
2026-09-13 16:36:07 +0300

    добавил volumes в app и проестил в main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 from contextlib import asynccontextmanager
 from typing import Annotated
+
 from fastapi import FastAPI, Depends
+import psycopg2
 
 from src.db.database import get_db, init_pool, close_pool
-
-from sqlalchemy import text
-from sqlalchemy.orm import Session
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -19,8 +18,17 @@ app = FastAPI(lifespan=lifespan)
 def test_endpoint():
     return "Hello"
 
+@app.get('/test')
+def fun(db: Annotated[psycopg2.extensions.connection, Depends(get_db)]):
+    with db.cursor() as cur:
+        cur.execute("SELECT * FROM departments")
+        rows = cur.fetchall()
+    return {"result": rows}
+
 @app.get('/health')
-def health_check(db: Annotated[Session, Depends(get_db)]):
-    a = db.execute(text('SELECT 1'))
-    return {'status': 'db connected',
-            'var': a}
+def health_check(db: Annotated[psycopg2.extensions.connection, Depends(get_db)]):
+    with db.cursor() as cur:
+        cur.execute("SELECT * FROM majors")
+        rows = cur.fetchall()
+    return {"status": "db connected",
+            "result": rows}
